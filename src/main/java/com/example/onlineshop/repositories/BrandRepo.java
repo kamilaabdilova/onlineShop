682f46fa60294e5d5f649c4005e4e831c7113092
Akmatovedil <akmatov.ed@gmail.com>
2023-07-14 14:41:30 +0300

    Spring security, jwt token исправил регистрацию

--- src/main/java/com/example/onlineshop/repositories/BrandRepo.java
+++ src/main/java/com/example/onlineshop/repositories/BrandRepo.java
@@ -4,7 +4,10 @@ import com.example.onlineshop.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BrandRepo extends JpaRepository<Brand, Long> {
+    Optional<Brand> findByNameBrand(String nameBrand);
 
 }
